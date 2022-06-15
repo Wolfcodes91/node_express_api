ba51f3c01092d45dc215de0817f9379b634fbdc7
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-06-15 15:59:04 -0700

    done

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { createUser, getUser, getUsers, deleteUser, updateUser } from '../controllers/users.js';
+
+
+const router = express.Router();
+
+// all routers here begin with /users
+router.get('/', getUsers);
+
+router.post('/', createUser)
+
+router.get('/:id', getUser)
+
+router.delete('/:id', deleteUser)
+
+router.patch('/:id', updateUser)
+
+export default router;
